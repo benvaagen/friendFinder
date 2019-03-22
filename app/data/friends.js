473192270028db63859b2c5f15d01a497e64e878
bvaagen <benvaagen24@gmail.com>
2019-03-21 23:48:31 -0700

    more code, but not done yet... and so forth...

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Dan Levy",
+        photo: "https://i.pinimg.com/originals/0e/81/ec/0e81ecfcd6008bf6a6ebc2d8ed410f85.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        name: "Chris Hemsworth",
+        photo: "https://m.media-amazon.com/images/M/MV5BOTU2MTI0NTIyNV5BMl5BanBnXkFtZTcwMTA4Nzc3OA@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        scores: [4, 2, 3, 5, 5, 1, 1, 4, 4, 5]
+    },
+    {
+        name: "Dustin Miligan",
+        photo: "https://the-hollywood-gossip-res.cloudinary.com/iu/s--ucnUmQQv--/t_full/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1364524910/dustin-milligan-pic.jpg",
+        scores: [5, 1, 2, 4, 3, 2, 4, 5, 4, 3]
+    },
+    {
+        name: "Noah Reid",
+        photo: "http://www.tarragontheatre.com/wp-content/uploads/2017/11/noah-reid-250x300.jpg",
+        scores: [2, 4, 4, 5, 3, 1, 1, 2, 4, 5]
+    },
+    {
+        name: "Justin Timberlake",
+        photo: "https://www.kxl.com/wp-content/uploads/2018/01/timberlake.jpg",
+        scores: [1, 1, 2, 5, 4, 3, 2, 1, 5, 5]
+    },
+    {
+        name: "Rob Gronkowski",
+        photo: "https://sportshub.cbsistatic.com/i/r/2018/10/04/7f792aac-998f-4b7a-bd78-5486be778a5a/thumbnail/770x433/c07e34520c2bca0c0e39dfe7b35b4202/gronk.jpg",
+        scores: [5, 5, 4, 4, 3, 3, 2, 2, 1, 1]
+    }
+];
+
+module.exports = friends;
