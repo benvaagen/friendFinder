74ba15656700a23e640420037e812fb78b547a72
bvaagen <benvaagen24@gmail.com>
2019-03-23 12:25:30 -0700

    got the code working, now I have some polishing work to do, and so forth...

--- README.md
+++
@@ -1,2 +0,0 @@
-# friendFinder
-node.js &amp; express
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,45 @@
 var friends = [
     {
-        name: "Dan Levy",
-        photo: "https://i.pinimg.com/originals/0e/81/ec/0e81ecfcd6008bf6a6ebc2d8ed410f85.jpg",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+      name: "Dan Levy",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BNWEyOGFlNzktMWQyNC00NTA0LWJjOTUtMjQzMmFjMTllOGY0XkEyXkFqcGdeQXVyMTY0MTA3MDQ@._V1_UY317_CR174,0,214,317_AL_.jpg",
+      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
     },
     {
-        name: "Chris Hemsworth",
-        photo: "https://m.media-amazon.com/images/M/MV5BOTU2MTI0NTIyNV5BMl5BanBnXkFtZTcwMTA4Nzc3OA@@._V1_UX214_CR0,0,214,317_AL_.jpg",
-        scores: [4, 2, 3, 5, 5, 1, 1, 4, 4, 5]
+      name: "Dustin Miligan",
+      photo:
+        "http://www.gstatic.com/tv/thumb/persons/299377/299377_v9_bb.jpg",
+      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
     },
     {
-        name: "Dustin Miligan",
-        photo: "https://the-hollywood-gossip-res.cloudinary.com/iu/s--ucnUmQQv--/t_full/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1364524910/dustin-milligan-pic.jpg",
-        scores: [5, 1, 2, 4, 3, 2, 4, 5, 4, 3]
+      name: "Noah Reid",
+      photo:
+        "http://www.gstatic.com/tv/thumb/persons/173363/173363_v9_bb.jpg",
+      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
     },
     {
-        name: "Noah Reid",
-        photo: "http://www.tarragontheatre.com/wp-content/uploads/2017/11/noah-reid-250x300.jpg",
-        scores: [2, 4, 4, 5, 3, 1, 1, 2, 4, 5]
+      name: "Sarah Levy",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BN2E1YWYxM2ItNDMxMS00YjQwLTkxYTgtNTQ0MTMzN2RjMjU0L2ltYWdlXkEyXkFqcGdeQXVyMTk2NTk2OTM@._V1_UY317_CR131,0,214,317_AL_.jpg",
+      scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
     },
     {
-        name: "Justin Timberlake",
-        photo: "https://www.kxl.com/wp-content/uploads/2018/01/timberlake.jpg",
-        scores: [1, 1, 2, 5, 4, 3, 2, 1, 5, 5]
+      name: "Emily Hampshire",
+      photo:
+        "http://image.tmdb.org/t/p/w342/8KYPzjQFy1sVZgnsEDV5bHb3DlP.jpg",
+      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
     },
     {
-        name: "Rob Gronkowski",
-        photo: "https://sportshub.cbsistatic.com/i/r/2018/10/04/7f792aac-998f-4b7a-bd78-5486be778a5a/thumbnail/770x433/c07e34520c2bca0c0e39dfe7b35b4202/gronk.jpg",
-        scores: [5, 5, 4, 4, 3, 3, 2, 2, 1, 1]
-    }
-];
-
-module.exports = friends;
+      name: "Annie Murphy",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BMTk3MTA2ODQ1MF5BMl5BanBnXkFtZTcwMjkwMDYxNw@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+      scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    },
+    {
+      name: "Jennifer Roberston",
+      photo:
+        "http://www1.pictures.zimbio.com/gi/Jennifer+Robertson+24th+Annual+Critics+Choice+Hi_JJek1mVSl.jpg",
+      scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    },
+  ];
+  module.exports = friends;
